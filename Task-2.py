8ca9d3965966f0dec1a298a228f5a37605e0869a
Sai-0-1 <erupakasaireddy@gmail.com>
2025-07-23 09:47:14 +0530

    Update Task-2.py

---
+++ Task-2.py
@@ -0,0 +1,8 @@
+import math
+n = float(input("Enter a number: "))
+sqrt_n = math.sqrt(n)
+log_n = math.log(n)
+sin_n = math.sin(n)
+print(f"The square root of {n} is {sqrt_n:.2f}")
+print(f"The natural logarithm (log base e) of {n} is {log_n:.2f}")
+print(f"The sine of {n} (in radians) is {sin_n:.2f}")
